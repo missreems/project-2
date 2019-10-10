1857d062722bde1ac122701702f0f948a4816896
Reema Patel <reemapatel17@outlook.com>
2019-10-10 11:04:23 +0100

    update

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './components/Home'
+import MoviesIndex from './components/MoviesIndex'
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <main>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/movies">Index</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/movies" component={MoviesIndex} />
+          </Switch>
+        </main>
+      </BrowserRouter> 
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ src/components/MoviesIndex.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MoviesIndex extends React.Component {
+  render() {
+    return (
+      <h1>Index Page</h1>
+    )
+  }
+}
+
+export default MoviesIndex
